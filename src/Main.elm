e57f1fc1c7270a3166809f0b110e62729769dea9
Tanner Hoelzel <thoelze1@binghamton.edu>
2022-10-12 12:06:23 -0500

    static layers (each with its own state)
    So happy to finally have something working

--- src/Main.elm
+++ src/Main.elm
@@ -45,7 +45,7 @@ main =
         }
 
 type alias Model =
-    { layer : Int -> Int
+    { layers : List (Int -> Bool -> (Maybe Int,Bool) , Bool)
     }
 
 
@@ -56,24 +56,58 @@ type Msg
 type alias Flags =
     ()
 
-foo : Int -> Int
-foo _ = 3
+baseMap : Int -> Bool -> (Maybe Int,Bool)
+baseMap i _ =
+    case i of
+        9 -> (Just 8,False)
+        _ -> (Just i,False)
+
+-- would be cooler if there was pttern matching on functions...
+-- shiftLayer 6 b = (Nothing,not b)
+-- and
+-- shiftLayer i True = (Just 3,b)
+-- and
+-- shiftLayer i False = (Just i,b)
+-- elm really is dumb haskell
+shiftLayer : Int -> Bool -> (Maybe Int,Bool)
+shiftLayer i b =
+    if i == 6 then
+        (Nothing,not b)
+      else if b then
+        (Just 3,b)
+      else
+        (Just i,b)
 
 init : Flags -> ( Model, Cmd Msg )
 init _ =
-    ( { layer = identity } , Cmd.none )
+    ( { layers = [ (baseMap , False)
+                , (shiftLayer , False)
+                ]
+      }
+      , Cmd.none )
+
+doOps : Maybe Int -> Model -> (Model, Maybe Int)
+doOps i m =
+  case m.layers of
+    [] -> ({ layers = [] },i)
+    ((l,s)::rest) -> case i of
+                       Nothing -> (m,Nothing)
+                       Just x -> let
+                                   (output,updatedState) = (l x s)
+                                   (updatedRest,finalOutput) = doOps output
+                                                                     { layers = rest }
+                                 in
+                                 ({ layers = (l,updatedState) :: updatedRest.layers }
+                                  ,finalOutput)
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    let
-        transform : InputType -> OutputType
-        transform k = Just (model.layer k)
-    in
-    case msg of
-        Input 9 -> ( { layer = foo }, Cmd.none )
-        Input 8 -> ( model, loopback (Just 7) )
-        Input input -> ( model, put (transform input))
-
+  case msg of
+    Input i ->
+      let
+          (newModel,output) = doOps (Just i) model
+      in
+      (newModel, put output)
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
