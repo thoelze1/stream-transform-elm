64ec13cec632a9edb630b321d7aa638e1cc3bad7
Tanner Hoelzel <thoelze1@binghamton.edu>
2022-10-18 17:16:33 -0500

    store events in the layer state
    I think that the repeated output from, e.g. 5 1 2 3 4 5 (results in 5
    tens) has to do with the JS<->Elm ports and/or node.repl as opposed to
    a bug in `doOps`

--- src/Main.elm
+++ src/Main.elm
@@ -44,8 +44,21 @@ main =
         , subscriptions = subscriptions
         }
 
+-- before I can implement tap-next-release, I need to "catch" events
+-- in a layer's state. that means first storing events as state and
+-- second emitting multiple events somehow (either at once as a List
+-- Int instead of Maybe Int, or in a "hacky" way by emitting control
+-- events back to the layer with the stored values)
 
-type alias Model = List (Int -> Bool -> (Maybe Int,Bool) , Bool)
+-- Ideas
+-- 1 Construct an algebra of state operations and allow the layer to
+--   emit a list of state operations
+-- 2 Pass the entire state to the layer and allow direct manipulation
+-- 3 Allow the layer to "replace itself." I think this didn't get me anywhere:
+--   Event -> State -> (List Event, (Event -> State -> (List Event))
+
+type alias LState = (Bool, List Int)
+type alias Model = List (Int -> LState -> (Maybe Int,LState) , LState)
 
 type alias Msg
     = Int
@@ -54,11 +67,11 @@ type alias Msg
 type alias Flags =
     ()
 
-baseMap : Int -> Bool -> (Maybe Int,Bool)
+baseMap : Int -> LState -> (Maybe Int,LState)
 baseMap i _ =
     case i of
-        9 -> (Just 8,False)
-        _ -> (Just i,False)
+        9 -> (Just 8,(False,[]))
+        _ -> (Just i,(False,[]))
 
 -- would be cooler if there was pttern matching on functions...
 -- shiftLayer 6 b = (Nothing,not b)
@@ -67,18 +80,39 @@ baseMap i _ =
 -- and
 -- shiftLayer i False = (Just i,b)
 -- elm really is dumb haskell
-shiftLayer : Int -> Bool -> (Maybe Int,Bool)
-shiftLayer i b =
+shiftLayer : Int -> LState -> (Maybe Int,LState)
+shiftLayer i (b,_) =
     if i == 6 then
-        (Nothing,not b)
+        (Nothing,(not b,[]))
       else if b then
-        (Just 3,b)
+        (Just 3,(b,[]))
       else
-        (Just i,b)
+        (Just i,(b,[]))
+
+catchLayer : Int -> LState -> (Maybe Int,LState)
+catchLayer i (b,xs) =
+    if b then
+        if i == 5 then
+            (Just (List.foldl (+) 0 xs),(False,[]))
+          else
+            (Nothing,(True,i::xs))
+      else
+        if i == 5 then
+            (Nothing,(True,[]))
+          else
+            (Just i,(False,[]))
+
+-- tnr : Int -> Model
+-- tnr i =
+--   [ \x b -> case x of
+--                i -> ]
 
 init : Flags -> ( Model, Cmd Msg )
 init _ =
-    ( [ (baseMap , False) , (shiftLayer , False) ] , Cmd.none )
+    ( [ (baseMap , (False,[]))
+      , (shiftLayer , (False,[]))
+      , (catchLayer , (False,[]))
+      ] , Cmd.none )
 
 doOps : Maybe Int -> Model -> (Model, Maybe Int)
 doOps i m =
