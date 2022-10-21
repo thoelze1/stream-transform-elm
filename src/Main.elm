dd759561ad70f1e11e5b524b8053f8db7db885ae
Tanner Hoelzel <thoelze1@binghamton.edu>
2022-10-21 02:46:29 -0500

    tap next release works!

--- src/Main.elm
+++ src/Main.elm
@@ -28,6 +28,9 @@ import Platform exposing (Program)
 import String
 import Task
 import Process
+import Char
+import Tuple
+import Dict
 
 -- type alias InputType = Int
 -- type alias OutputType = Maybe Int
@@ -135,35 +138,114 @@ catchLayer c (b,xs) =
             ([c],(False,[]))
 
 -- this too should simply be a map from ctrl value to list of control values,
--- with anothr function that turns that map into a full layer
+-- with another function that turns that map into a full layer
 waitMap : Ctrl -> LState -> (List Ctrl, LState)
 waitMap (Ctrl d w i) s =
   case i of
     9 -> ([Ctrl d 1000 i],s)
     _ -> ([Ctrl d w i],s)
-  
--- tnr : Int -> Model
--- tnr i =
---   [ \x b -> case x of
---                i -> ]
 
+-- next we've got to make this more general: mkTapNextRelease, isPress, isRelease, etc
+
+isPress : Ctrl -> Bool
+isPress c = modBy 2 (getInt c) == 0
+
+isDown : LState -> Bool
+isDown (_,xs) = List.member 0 (List.map getInt xs)
+
+codes : List Int
+codes = List.range 65 90
+
+chars : List Char
+chars = List.map Char.fromCode codes
+
+zip : List a -> List b -> List (a,b)
+zip xs ys = List.map2 Tuple.pair xs ys
+
+codesByChar = Dict.fromList (zip chars codes)
+charsByCode = Dict.fromList (zip codes chars)
+
+charToCode : Char -> Maybe Int
+charToCode c = Dict.get c codesByChar
+
+codeToChar : Int -> Maybe Char
+codeToChar i = Dict.get i charsByCode
+
+-- i need to rewrite LState to be algebraic and then customize the tnr datatype
+-- to include a second boolean for whether or not the key has been pressed yet
+
+-- let's say that even integers x represent key presses, and each x+1 represents
+-- release of the key of which x represents a press. Let's say 0 and 1 represent
+-- press and release of key A
+tapNextReleaseA : Ctrl -> LState -> (List Ctrl, LState)
+tapNextReleaseA c (isHold,xs) =
+-- b corresponds to whether or not map is active
+  if not (isDown (isHold,xs)) then
+      if getInt c == 0 then
+          ([],(False,[c]))
+        else
+          ([c],(False,[]))
+    else
+      if not isHold then
+          if isPress c then
+              ([],(False,c::xs))
+            else
+              if getInt c == 1 then
+                  (c::xs,(False,[]))
+                else
+                  if List.member ((getInt c)-1) (List.map getInt xs) then
+                      let (zero,rest) = List.partition (\x -> getInt x == 0) (c::xs) in
+                      (List.append (List.map (\z -> map (\q -> 3*q) z) rest)
+                                   zero
+                      ,(True,zero))
+                    else
+                      ([c],(False,xs))
+        else
+          if getInt c == 1 then
+              ([c],(False,[]))
+            else
+              ([map (\z -> 3*z) c],(True,xs))
+
+--  if isPress c then
+--      if getInt c == 0 then
+--          ([],(False,[c]))
+--        else
+--          if isHold then
+--              ([map (\z -> 3*z) c],(isHold,xs))
+--            else
+--              ([],(False,c::xs))
+--    else
+--      if isDown (isHold,down) then
+--          if List.member ((getInt c)-1) (map getInt xs) then
+--              (c::xs,(True,[]))
+--            else
+--              ([c],(False,[]))
+
+-- b corresponds to whether or not map is active
+--  if isDown (b,xs) then
+--      if isPress c then
+--          if isHold then ([map (\z -> 3*z) c],(True,xs)) else ([],(False,c::xs))
+--        else
+--          if List.member ((getInt c)-1) (map getInt xs) then (c::xs,(True,[]))
+--    else if getInt c == 0
+--      ([],(False,[c]))
+--    else
+--      ([c],(False,[]))
 
 type alias Layer = Ctrl -> LState -> (List Ctrl, LState)
 
 myMap : List Layer
-myMap = [ baseMap
-        , shiftLayer
-        , catchLayer
-        , waitMap
-        , loopMap
-        ]
+myMap = [ tapNextReleaseA ]
         
--- here there should be a function to give a map the default state,
--- so it becomes (initialize [baseMap,shiftLayer,catchLayer,loopMap,waitMap])
 init : Flags -> ( Model, Cmd Msg )
 init _ =
     ( List.map (\l -> (l , (False,[]))) myMap, Cmd.none )
 
+
+-- i think doOps should manage destination and waiting of events;
+-- each layer therefore should only care about the actual event it
+-- has recevied, because it should have been provided the event at the
+-- correct time (and should be the correct recipient) of the event
 doOps : List Ctrl -> Model -> (Model, List Ctrl)
 doOps i m =
   case m of
@@ -184,6 +266,12 @@ doOps i m =
           in
           ((l,finalLayerState)::newRestState,
           List.append finalOutput1 finalOutput2)
+
+-- there are actually 3 ways to do loopback:
+-- 1 using ports with a JS listener
+-- 2 through the Elm runtime with a Cmd Msg
+-- 3 recursion
+-- 4 through the Elm runtime using the model
 
 -- i think I can remove the loopback port...
 -- the following map belongs as data (a list of tuples?), not as code
