f9da58cab0b413d6380418d232dd1ecfa97f626a
Tanner Hoelzel <thoelze1@binghamton.edu>
2022-10-27 22:56:16 -0500

    add zip to utils

--- src/Utils.elm
+++ src/Utils.elm
@@ -1,4 +1,7 @@
-module Utils exposing (const)
+module Utils exposing (const,zip)
 
 const : a -> b -> a
 const x _ = x
+
+zip : List a -> List b -> List (a,b)
+zip xs ys = List.map2 Tuple.pair xs ys
